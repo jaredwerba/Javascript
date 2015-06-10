45e59f718ff770d8c56fa2b5ae58a1aae6393ec2
jaredwerba <jwerba@gmail.com>
2015-06-10 15:19:06 -0400

    if else conditons

--- weather.js
+++ weather.js
@@ -2,9 +2,22 @@ var user = prompt("what is the weather like today?").toUpperCase();
 
 switch (user){
     case 'RAINY':
+
+        if('RAINY' && 'SUNNY'){
+            console.log("how is that possible");
+        }
+        else{
+            console.log("whats going on here");
+        }
         console.log("I like the rain");
         break;
     case 'SUNNY':
+        if ('SUNNY' || 'SNOWING'){
+            console.log("I hope there isn't any ice");
+        }
+        else{
+            console.log("wtf");
+        }
         console.log("Get some Vitamin D");
         break;
     case 'SNOWING':
