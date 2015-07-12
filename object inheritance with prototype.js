90e54422163f6f16fbd89def7b7bb8d1551cbea8
jaredwerba <jwerba@gmail.com>
2015-07-12 19:04:11 -0400

    inheriting from multiple classes

--- object inheritance with prototype.js
+++ object inheritance with prototype.js
@@ -13,3 +13,30 @@ buddy.bark();
 var snoopy = new Dog("Beagle");
 /// this time it works!
 snoopy.bark();
+
+
+
+// original classes
+function Animal(name, numLegs) {
+    this.name = name;
+    this.numLegs = numLegs;
+    this.isAlive = true;
+}
+function Penguin(name) {
+    this.name = name;
+    this.numLegs = 2;
+}
+function Emperor(name) {
+    this.name = name;
+    this.saying = "Waddle waddle";
+}
+
+// set up the prototype chain
+Penguin.prototype = new Animal();
+Emperor.prototype = new Penguin();
+
+var myEmperor = new Emperor("Jules");
+
+console.log(myEmperor.saying); // should print "Waddle waddle"
+console.log(myEmperor.numLegs); // should print 2
+console.log(myEmperor.isAlive); // should print true
