f785f4973517edd07a0d4b4153661c95d42c62cd
jaredwerba <jwerba@gmail.com>
2015-07-12 20:07:46 -0400

    access private with parameter

--- object inheritance with prototype.js
+++ object inheritance with prototype.js
@@ -90,3 +90,23 @@ console.log(john.returnBalance);
 var myBalanceMethod = john.askTeller();
 var myBalance = myBalanceMethod();
 console.log(myBalance);
+//
+// // Passing Arguments
+// The askTeller function has been modified within the Person class to directly give you your balance. However, it now needs the account password in order to return the bankBalance.
+
+function Person(first,last,age) {
+   this.firstname = first;
+   this.lastname = last;
+   this.age = age;
+   var bankBalance = 7500;
+
+   this.askTeller = function(pass) {
+     if (pass == 1234) return bankBalance;
+     else return "Wrong password.";
+   };
+}
+
+var john = new Person('John','Smith',30);
+/* the variable myBalance should access askTeller()
+   with a password as an argument  */
+var myBalance = john.askTeller(1234)
